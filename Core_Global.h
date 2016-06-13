6f5ccdc7f22a10376b8deaf93af8f9a87c6d0bad
Vcanccc <wwc7033@163.com>
2016-06-13 22:08:37 +0800

    Create Core_Global.h

---
+++ Core_Global.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef _CORE_GLOBAL_H_
+#define _CORE_GLOBAL_H_
+
+#define DIRECTINPUT_VERSION 0x0800
+
+// Windows includes
+#include <windows.h>
+
+// Standard ANSI-C includes
+#include <stdio.h>
+
+// DirectX includes
+#include "d3d9.h"
+#include "d3dx9.h"
+#include "dsound.h"
+#include "dinput.h"
+#include "dshow.h"
+
+// Core includes
+#include "Core_System.h"
+#include "Core_Graphics.h"
+
+#endif
